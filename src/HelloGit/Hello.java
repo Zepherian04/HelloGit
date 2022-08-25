fec7e1f2bd317febf08a5b75601867880409a503
Dawson <Dawson@LAPTOP-4Q7UMP1O>
2022-08-25 11:35:20 -0500

    added my major

--- src/HelloGit/Hello.java
+++ src/HelloGit/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello, I am Dawson McGahan");
+		System.out.println("Hello, I am Dawson McGahan\n");
+		System.out.println("My Major is Computer Engineering\n");
 
 	}
 
