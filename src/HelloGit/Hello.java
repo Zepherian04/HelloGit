dc39ad1a177afe44545a1ca7a185fb6ed6c6d5ef
thomaswaller <thomaswaller@thomass-mbp-6.unl.edu>
2022-08-28 18:09:46 -0500

    Added name and major

--- src/HelloGit/Hello.java
+++ src/HelloGit/Hello.java
@@ -6,6 +6,8 @@ public class Hello {
 		
 		System.out.println("Hello, I am Dawson McGahan\n");
 		System.out.println("My Major is Computer Engineering\n");
+		System.out.println("Hi, I'm Thomas Waller");
+		System.out.println("my major is computer science");
 
 	}
 
